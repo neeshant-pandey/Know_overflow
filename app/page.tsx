607321cf8461bd6809a2d6a252e6edb1147e7535
neeshant <neeshantpandeydas@gmail.com>
2023-10-26 19:12:21 +0530

    Added Authentication

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Nextjs Hare Krishna</h1>{" "}
-      <h2 className="h2-bold">Nextjs Hare Krishna</h2>{" "}
-      <h3 className="h3-bold">Nextjs Hare Krishna</h3>{" "}
-    </div>
-  )
-}
